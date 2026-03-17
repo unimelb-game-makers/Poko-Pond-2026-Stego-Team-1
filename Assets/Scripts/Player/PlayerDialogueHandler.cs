29bf16aef7d3eb92d044c174da3bd82597a1509f
Elytride <zonesky634@gmail.com>
2026-03-18 00:12:42 +1100

    Feat: added scripts, sfx and prefab for dialogue system. E to call dialogue; Z to go through dialogue.

---
+++ Assets/Scripts/Player/PlayerDialogueHandler.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+// Add to the Player prefab alongside PlayerMovement.
+// Disables PlayerMovement and zeroes velocity while dialogue is open, then re-enables it when done.
+[RequireComponent(typeof(PlayerMovement))]
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerDialogueHandler : MonoBehaviour
+{
+    private PlayerMovement _movement;
+    private Rigidbody2D    _rb;
+
+    private void Awake()
+    {
+        _movement = GetComponent<PlayerMovement>();
+        _rb       = GetComponent<Rigidbody2D>();
+    }
+
+    // Subscribes to dialogue events
+    private void OnEnable()
+    {
+        EventManager.OnDialogueStart += OnDialogueStart;
+        EventManager.OnDialogueEnd   += OnDialogueEnd;
+    }
+
+    // Unsubscribes from dialogue events
+    private void OnDisable()
+    {
+        EventManager.OnDialogueStart -= OnDialogueStart;
+        EventManager.OnDialogueEnd   -= OnDialogueEnd;
+    }
+
+    // Freezes the player when dialogue starts
+    private void OnDialogueStart(DialogueData _)
+    {
+        _movement.enabled = false;
+        _rb.velocity      = Vector2.zero;
+    }
+
+    // Unfreezes the player when dialogue ends
+    private void OnDialogueEnd() => _movement.enabled = true;
+}
